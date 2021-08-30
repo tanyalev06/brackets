29cc9c40e6e39d6828cd974520e1ee8241f4e56d
Tatyana Levchenko <grib.tanya06@mail.ru>
2021-08-31 00:47:17 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strBracket = bracketsConfig.flat().join("");
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+      if(str[i] === " ") continue;
+      let ind = strBracket.indexOf(str[i])
+      if (ind % 2 === 0) {          
+          if (str[i] === strBracket[ind + 1] && stack[stack.length - 1] === ind) stack.pop();
+          else if (str[i] === strBracket[ind + 1] && stack[stack.length - 1] !== ind) stack.push(ind);
+          else stack.push(ind);
+      } 
+      else if(stack.pop() !== ind-1) return false;           
+  }
+  return stack.length === 0
 }
